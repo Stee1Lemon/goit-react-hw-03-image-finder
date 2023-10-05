b7c577e0ccf4cac16e261ae4ae3cd5d8ead79834
Stee1Lemon <121914099+Stee1Lemon@users.noreply.github.com>
2023-10-05 14:11:36 +0100

    conponents-fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,8 @@ import { Component } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { ButtonMore } from './Button/Button';
-import { Loader } from './Loader/Loader';
-import { Modal } from './Modal/Modal';
+// import { Loader } from './Loader/Loader';
+// import { Modal } from './Modal/Modal';
 
 export class App extends Component {
   render() {
